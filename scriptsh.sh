fe5703d6e85c008c8953f54822c970cf05cd2cea
MatheusPatricio013 <99891939+MatheusPatricio013@users.noreply.github.com>
2022-11-28 12:25:03 -0300

    Update scriptsh.sh

--- scriptsh.sh
+++ scriptsh.sh
@@ -103,6 +103,9 @@ if [ $? -eq 0 ]
 		git clone https://github.com/BH-Beehive/dockerfileForMysql.git
 		
 		cd dockerfileForMysql
+		sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+		
+		sudo chmod +x /usr/local/bin/docker-compose
 		
 		sudo docker-compose up -d
 		
